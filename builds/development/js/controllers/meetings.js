a29376aa76e9e8ea001e156c86bdf052e504e2ca
Ignacio Mieites <ia.mieites@gmail.com>
2015-02-13 18:36:25 -0300

    meetings number watched and updated on the nav view.

--- builds/development/js/controllers/meetings.js
+++ builds/development/js/controllers/meetings.js
@@ -1,13 +1,28 @@
 myApp.controller('MeetingsController', 
-    function($scope, $firebase){
+    function($scope, $firebase, $rootScope){
     
     var ref = new Firebase('https://attendancetestapp.firebaseio.com/meetings');
-    var meetings = $firebase(ref);
+    var meetingsInfo = $firebase(ref);
+    var meetingsObj = $firebase(ref).$asObject();    
+    var meetingsArray = $firebase(ref).$asArray();
 
-    $scope.meetings = meetings.$asObject();
+    meetingsObj.$loaded().then(function(data){
+        $scope.meetings = meetingsObj;
+        console.log('meetings.js: $scope.meetings set to...');
+        console.log($scope.meetings);
+    }); // meetingsObj loaded
+
+    meetingsArray.$loaded().then(function(data){
+        $rootScope.howManyMeetings = meetingsArray.length;
+    }); // meetingsArray loaded
+
+    meetingsArray.$watch(function(event){
+        $rootScope.howManyMeetings = meetingsArray.length;
+    }); // meetingsArray watch
 
     $scope.addMeeting = function(){
-        meetings.$push({
+
+        meetingsInfo.$push({
             name: $scope.meetingname,
             date: Firebase.ServerValue.TIMESTAMP
         }).then( function() {
@@ -16,7 +31,7 @@ myApp.controller('MeetingsController',
     } // addMeeting
 
     $scope.deleteMeeting = function(key){
-        meetings.$remove(key);
+        meetingsInfo.$remove(key);
     }
     
 }); //MeetingsController
